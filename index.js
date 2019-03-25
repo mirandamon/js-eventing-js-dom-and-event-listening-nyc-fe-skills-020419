895d3a12bb200d9f9fab120bca941a164d077fd4
Nathan Miranda <nate845@gmail.com>
2019-03-25 23:41:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ const divs = document.querySelectorAll('div')
 for (let div of divs) {
   div.addEventListener('click', (event) => {
     console.log(event)
-    event.stopPropgation() // stops bubbling from happening
+    event.stopPropagation() // stops bubbling from happening
   })
 }
 
