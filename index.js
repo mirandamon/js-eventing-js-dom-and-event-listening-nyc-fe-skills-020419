7557eda410b1ef0075188f60a7425beebf713373
Nathan Miranda <nate845@gmail.com>
2019-03-25 22:36:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function onClick() {
 // document level events: online/offline, global help button/scrolling
 
 
-main.addEventListener('click', onClick)
+main.addEventListener('copy', onClick)
 
 document.addEventListener('click', onClick)
