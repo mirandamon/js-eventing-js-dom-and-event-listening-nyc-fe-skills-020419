ee9a6cab9ca8c3e726f7117c6b72986cd382e706
Nathan Miranda <nate845@gmail.com>
2019-03-25 23:31:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ button.addEventListener('click', () => {
   const newP = document.createElement('p')
   // <p>Congratulations!</p>
   newP.innerHTML = 'Congratulations!'
-  document.body.appendChild()
+  document.body.appendChild(newP)
   // <body> <div> </div> <span></span> <p>Congratulations</p> </body>
 })
 
